2b2318881b47d8b566b132959b7a969972700f4f
Dzmitry Stolbou <persik1223@gmail.com>
2020-03-21 16:36:13 +0300

    feat: add decode func

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let size = expr.length / 10;
+    let morse_char, res = '', tmp_box, f = 0;
+    for (let i = 0; i < size; i++){
+        tmp_box = expr.substring(i*10,(i+1)*10);
+        morse_char='';
+        f = 0;
+        for (let j = 0; j < 10; j++){
+            if (tmp_box.charAt(j) == '*') {res+=' '; f = 1; break;} 
+            else {
+                if (tmp_box.charAt(j) == '0'){}
+                else{
+                    if (tmp_box.charAt(j) == '1') {
+                        let m = j+1;
+                        if (tmp_box.charAt(m) == '1') {morse_char+='-'; j++;}
+                        else {if (tmp_box.charAt(m) == '0') {morse_char+='.';}}
+                    }
+                }
+            }
+        }
+        if (f == 0) res+=MORSE_TABLE[morse_char];
+    }
+    return res;
 }
 
 module.exports = {
